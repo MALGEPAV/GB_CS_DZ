5d3bae354e76dcb87cd16379767155d7409065f3
MALGEPAV <malgepav@gmail.com>
2022-11-28 22:29:22 +0300

    DZ3 MALGEPAV

---
+++ DZ3/zadacha19HARD/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Write("Введите натуральное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string reply = "";
+if (IsPalindrome(n))
+{
+    reply = "Число является палиндромом.";
+}
+else
+{
+    reply  = "Число НЕ является палиндромом";
+}
+System.Console.WriteLine(reply);
+//-----------ФУНКЦИЯ-ПРОВЕРКА--------------------------------------------------
+bool IsPalindrome(int number)
+{
+    bool answer = true;
+    //Находим количество цифр
+    int length = 1;
+    if (n>0)
+    {
+        length = Convert.ToInt32(Math.Floor(Math.Log10(number))) + 1;
+    }
+    for (int i = 1; i <= length / 2; i++)
+    {
+        //Цифра на позиции i считая слева:
+        int leftDigit = number / Convert.ToInt32(Math.Pow(10, length - i)) % 10;
+        //Цифра на позиции i считая справа:
+        int rightDigit = number / Convert.ToInt32(Math.Pow(10, i-1)) % 10;
+        if (leftDigit != rightDigit)
+        {
+            answer = false;
+            break;
+        }
+    }
+    return answer;
+}
+
+
---
+++ DZ3/zadacha21HARD/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.Write("Введите размерность пространства: ");
+int dim = Convert.ToInt32(Console.ReadLine());
+int[] x = new int[dim];
+int[] y = new int[dim];
+Console.WriteLine("Введите координаты первой точки:");
+for (int i = 0; i < dim; i++)
+{
+    Console.Write("x{0} = ", i + 1);
+    x[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("Введите координаты второй точки:");
+for (int i = 0; i < dim; i++)
+{
+    Console.Write("y{0} = ", i + 1);
+    y[i] = Convert.ToInt32(Console.ReadLine());
+}
+double r = Distance(x, y);
+Console.WriteLine("Расстояние между точками {0} и {1} равно {2}", 
+CoordinatesDisplay(x),CoordinatesDisplay(y),r);
+//----------------фУНКЦИЯ-РАССТОЯНИЕ---------------------
+double Distance(int[] x, int[] y)
+{
+    double r = 0;
+    for (int j = 0; j < x.Length; j++)
+    {
+        r += (y[j] - x[j]) * (y[j] - x[j]);
+    }
+    r = Math.Sqrt(r);
+    return r;
+}
+//----------------фУНКЦИЯ-ВЫВОД-КООРДИНАТ-ТОЧКИ---------------------
+string CoordinatesDisplay(int[] x)
+{
+    string coordinates = "(";
+    for (int k = 0; k < x.Length-1; k++)
+    {
+        coordinates +=  x[k].ToString()+",";
+    }
+    coordinates +=  x[^1].ToString()+")";
+    return coordinates;
+}
