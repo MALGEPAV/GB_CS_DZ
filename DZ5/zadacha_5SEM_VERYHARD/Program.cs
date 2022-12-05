30e741f8418dbf37806a9fd1fa35bb59806093fb
MALGEPAV <malgepav@gmail.com>
2022-12-05 19:53:22 +0300

    DZ5 MALGEPAV

---
+++ DZ5/zadacha_5SEM_VERYHARD/Program.cs
@@ -0,0 +1,133 @@
+﻿System.Console.WriteLine("Проверка:");
+int[] testArray1 = { 1, 5, 2, 3, 4, 1, 7, 8, 15, 1 };
+PrintArray(testArray1);
+PrintAnswer(testArray1);
+
+int[] testArray2 = { 1, 5, 2, 3, 4, 6, 1, 7};
+PrintArray(testArray2);
+PrintAnswer(testArray2);
+
+int[] testArray3 = {1, 5, 3, 4, 1, 7, 8 , 15 , 1};
+PrintArray(testArray3);
+PrintAnswer(testArray3);
+
+int[] testArray4 = {5, 7, 9};
+PrintArray(testArray4);
+PrintAnswer(testArray4);
+
+int[] testArray5 = {1, 3, 3, 3};
+PrintArray(testArray5);
+PrintAnswer(testArray5);
+
+int[] testArray6 = {1, 1, 1, 1};
+PrintArray(testArray6);
+PrintAnswer(testArray6);
+
+int[] testArray7 = {1};
+PrintArray(testArray7);
+PrintAnswer(testArray7);
+System.Console.WriteLine("---------------------------------------------------------------");
+
+Console.Write("Введите длину случайного массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateRandomArray(n);
+Console.WriteLine("Ваш массив cлучайных чисел от 1 до {0}: ", n + n/2);
+PrintArray(array);
+PrintAnswer(array);
+
+//-----------------------------------------------------------------------------------------
+//ФУНКЦИИ: СОЗДАНИЕ МАССИВА И ВЫВОД НА ЭКРАН
+int[] CreateRandomArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+        array[i] = new Random().Next(1, size + size/2+1); //от 1 до полторы длины массива
+    return array;
+}
+void PrintArray(int[] array)
+{
+    foreach (int el in array)
+        Console.Write($"{el} ");
+    Console.WriteLine();
+}
+//-------------------------------------------------------------------------------------------
+//ФУНКЦИЯ, КОТОРАЯ ПРОВЕРЯЕТ, ЕСТЬ ЛИ В МАССИВЕ ЧИСЛО НА ЕДИНИЦУ БОЛЬШЕ ДАННОГО
+bool NextNaturalPresent(int[] array, int value)
+{
+    bool answer = false;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == value + 1)
+        {
+            answer = true;
+            break;
+        }
+    }
+    return answer;
+}
+//-------------------------------------------------------------------------------------------
+//ФУНКЦИЯ, КОТОРАЯ ВЫДАЕТ МИНИМАЛЬНЫЙ МАКСИМАЛЬНЫЙ ЭЛЕМЕНТ В ДАННОМ МАССИВЕ
+void FindMin(int[] array, out int min, out int max)
+{
+    min = array[0];
+    max = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] < min) min = array[i];
+        if (array[i] > max) max = array[i];
+    }
+}
+//-------------------------------------------------------------------------------------------
+//ФУНКЦИЯ, КОТОРАЯ НА ВХОД ПРИНИМАЕТ МАССИВ, ИЩЕТ В НЕМ НАИБОЛЬШУЮ ПО ДЛИНЕ ПОСЛЕДОВАТЕЛЬНОСТЬ
+//ПОДРЯД ИДУЩИХ ЧИСЕЛ И ВОЗВРАЩАЕТ ПЕРВОЕ И ПОСЛЕДНЕЕ ЧИСЛО ЭТОЙ ПОСЛЕДОВАТЕЛЬНОСТИ
+void MaxSubsequenceBoundaries(int[] array, out int leftNumber, out int rightNumber)
+{
+    FindMin(array, out int min, out int max);
+    int currentNumber = min;
+
+    leftNumber = currentNumber; //итоговые левая и
+    rightNumber = currentNumber;//правая граница которые будут в ответе
+
+    int currentLeftNumber = currentNumber; //текущая лквая граница
+
+    int maxSequenceLength = 1; // максимальная длина серии в итоге
+    int currentSequenceLength = 1; //текущая длина серии
+
+    while (currentNumber < max)
+    {
+        currentLeftNumber = currentNumber; //возможно станет новой левой границей если серия будет длиннее 
+        currentSequenceLength = 1; // новая серия только начинается
+        while (NextNaturalPresent(array, currentNumber))//Пока в массиве есть следующее число, 
+        {                                               
+            currentNumber++;                            //переходим к нему
+            currentSequenceLength++;                    //и увеличиваем длину текущей серии
+        }
+        if (currentSequenceLength > maxSequenceLength) // Проверка стала ли новая серия длиннее прошлой
+        {
+            leftNumber = currentLeftNumber;  //если да, то обновляем левую границу
+            rightNumber = currentNumber;     // и правую
+            maxSequenceLength = currentSequenceLength;  // и длину максимальной серии
+        }
+        while ((!NextNaturalPresent(array, currentNumber)) && (currentNumber <= max)) //ищем начало новой серии
+        {                        //если у числа нет в массиве следующего и он не больше максимума
+            currentNumber++;  //берем следующее число
+        }        
+        currentNumber++;
+    }
+}
+//-----------------------------------------------------------------------------------------
+//ФУНКЦИЯ, ВЫДАЮЩАЯ ОТВЕТ ДЛЯ ДАННОГО МАССИВА
+void PrintAnswer(int[] array)
+{
+MaxSubsequenceBoundaries(array, out int leftNumber, out int rightNumber);
+if (leftNumber == rightNumber)
+{
+    Console.WriteLine("В массиве нет подряд идущих натуральных чисел.");
+}
+else
+{
+    Console.WriteLine("Наибольшая по длине последовательность натуральных чисел в этом массиве: [{0}, {1}]", leftNumber, rightNumber);
+}
+Console.WriteLine("----------------------------------------------------------");
+}
+
---
+++ DZ5/zadacha_HARD_STAT/Program.cs
@@ -0,0 +1,85 @@
+﻿Console.Write("Введите длину случайного массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+int[] array = CreateRandomArray(n);
+Console.WriteLine("Ваш массив cлучайных чисел от ({0}) до {1}: ", -n, n);
+PrintArray(array);
+double[] data = Info(array);
+Console.WriteLine();
+Console.WriteLine("Минимальный элемент в этом массиве равен {0} и имеет индекс {1}", data[0], data[1]);
+Console.WriteLine();
+Console.WriteLine("Максимальный элемент в этом массиве равен {0} и имеет индекс {1}", data[2], data[3]);
+Console.WriteLine();
+Console.WriteLine("Среднее арифметическое элементов массива равно {0:N2}", data[4]);
+Console.WriteLine();
+Console.WriteLine("Отсортированный массив:");
+PrintArray(array); //отсортировался внутри функции так как передался туда по ссылке
+Console.WriteLine();
+Console.WriteLine("Медиана равна {0:N2}", data[5]);
+
+//--------------------------------------------
+//ФУНКЦИИ: СОЗДАНИЕ МАССИВА И ВЫВОД НА ЭКРАН
+int[] CreateRandomArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+        array[i] = new Random().Next(-size, size + 1);
+    return array;
+}
+void PrintArray(int[] array)
+{
+    foreach (int el in array)
+        Console.Write($"{el} ");
+    Console.WriteLine();
+}
+//-------------------------------------------------------------------------------------------
+//ФУНКЦИЯ, КОТОРАЯ ВЫДАЕТ МИНИМАЛЬНЫЙ, МАКСИМАЛЬНЫЙ ЭЛЕМЕНТЫ, ИХ ИНДЕКСЫ, СРЕДНЕЕ
+//АРИФМЕТИЧЕСКОЕ И МЕДИАНУ
+double[] Info(int[] array)
+{
+    int n = array.Length;
+    int min = array[0];
+    int max = array[0];
+    int minIndex = 0;
+    int maxindex = 0;
+    double average = 0;
+    double median = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (array[i] < min)
+        {
+            min = array[i];
+            minIndex = i;
+        }
+        if (array[i] > max)
+        {
+            max = array[i];
+            maxindex = i;
+        }
+        average += array[i];
+    }
+    average /= n;
+    BubbleSort(array);
+
+    median = (double)(array[n / 2 + n % 2 - 1] + array[n / 2]) / 2; // иначе делит целочисленно почему-то
+    double[] data = { min, minIndex, max, maxindex, average, median};
+    return data;
+}
+//----------ФУНКЦИЯ-СОРТИРОВКА-ПУЗЫРЬКОМ--------
+void BubbleSort(int[] a)
+{
+    int temp = 0;
+    for (int i = a.Length - 1; i > 0; i--)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (a[j] < a[j + 1])
+            {
+                temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+            }
+        }
+    }
+}
