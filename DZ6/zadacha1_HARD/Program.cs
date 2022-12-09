44cac19e58108a4a81bf87b9c81be3837ac4d73e
MALGEPAV <malgepav@yandex.ru>
2022-12-10 00:48:36 +0300

    DZ6 MALGEPAV

---
+++ DZ6/zadacha1_HARD/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Write("Введите количество чисел правее и левее нуля: ");
+int n = Convert.ToInt32(Console.ReadLine());
+PrintNiggaFibonacci(n);
+Console.WriteLine();
+//----------------------------------------------------------------------------------
+void PrintNiggaFibonacci(int n)
+{
+    Console.WriteLine("Ваши числа негафибоначчи:\n");
+    if (n == 0) Console.WriteLine("0");
+    else if (n == 1) Console.WriteLine("1 0 1");
+    else
+    {
+        int[] fib = new int[2 * n + 1];
+        fib[n] = 0;
+        fib[n - 1] = 1;
+        fib[n + 1] = 1;
+        int nextLeftIndex = n-1;
+        int nextRightIndex = n+1;
+    
+        for (int i = 0; i < n-1; i++)
+        {
+            nextLeftIndex--;
+            nextRightIndex++;
+            fib[nextLeftIndex] = -fib[nextLeftIndex+1]+fib[nextLeftIndex+2];
+            fib[nextRightIndex] = fib[nextRightIndex-1]+fib[nextRightIndex-2];
+        }
+        PrintArray(fib);
+    }
+}
+void PrintArray(int[] array)
+{
+    foreach (int el in array)
+        Console.Write($"{el} ");
+    Console.WriteLine();
+}
---
+++ DZ6/zadacha2_HARD/Program.cs
@@ -0,0 +1,62 @@
+﻿Console.Write("Введите количество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+int[,] Matrix = new int[m, n];
+FillMatrix(Matrix);
+PrintMatrix(Matrix);
+Console.WriteLine();
+(int min, int minI, int minJ, int max, int maxI, int maxJ) = FindMinMax(Matrix);
+Console.WriteLine($"Минимальный элемент матрицы равен {min} и находится на позиции ({++minI}, {++minJ}).");
+Console.WriteLine();
+Console.WriteLine($"Максимальный элемент матрицы равен {max} и находится на позиции ({++maxI}, {++maxJ}).");
+Console.WriteLine();
+void PrintMatrix(int[,] M)
+{
+    for (int i = 0; i < M.GetLength(0); i++)
+    {
+        for (int j = 0; j < M.GetLength(1); j++)
+        {
+            Console.Write($"{M[i, j],3}\t");
+        }
+        Console.WriteLine();
+    }
+}
+void FillMatrix(int[,] M)
+{
+    for (int i = 0; i < M.GetLength(0); i++)
+    {
+        for (int j = 0; j < M.GetLength(1); j++)
+        {
+            M[i, j] = new Random().Next(-99, 100);
+        }
+    }
+}
+(int min, int minI, int minJ, int max, int maxI, int maxJ) FindMinMax(int[,] M)
+{
+    int minI = 0;
+    int minJ = 0;
+    int maxI = 0;
+    int maxJ = 0;
+    int min = M[0,0];
+    int max = M[0,0];
+    for (int i = 0; i < M.GetLength(0); i++)
+    {
+        for (int j = 0; j < M.GetLength(1); j++)
+        {
+            if (M[i,j]<min)
+            {
+                min = M[i,j];
+                (minI, minJ)= (i,j);  
+            }
+            if (M[i,j]>max)
+            {
+                max = M[i,j];
+                (maxI, maxJ)= (i,j);  
+            }
+        }
+    }
+
+return (min, minI, minJ, max, maxI,maxJ);
+}
